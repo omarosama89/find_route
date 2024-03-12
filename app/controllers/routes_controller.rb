9911b62ed3e57b5e5ed9e5024694a896b187e68a
Omar Osama <o.osama@youngcapital.nl>
2024-03-12 19:42:03 +0100

    PLS-0001 - Acceptance criteria: Return the cheapest direct sailing between origin port & destination port in following format. For example using CNSHA as origin port & NLRTM as destination port input parameters [   {     "origin_port": "CNSHA",     "destination_port": "NLRTM",     "departure_date": "2022-02-01",     "arrival_date": "2022-03-01",     "sailing_code": "ABCD",     "rate": "589.30",     "rate_currency": "USD"   } ]

---
+++ app/controllers/routes_controller.rb
@@ -0,0 +1,27 @@
+class RoutesController < ApplicationController
+  def index
+    result = GetCheapestEndpoint.new(
+      origin_port: origin_port, destination_port: destination_port, sailings: sailings
+    ).call
+
+    if result.success?
+      render json: result.value!.to_json, status: :ok
+    else
+      render json: { error: result.failure }, status: :bad_request
+    end
+  end
+
+  private
+
+  def origin_port
+    params[:origin_port]
+  end
+
+  def destination_port
+    params[:destination_port]
+  end
+
+  def sailings
+    SailingsBuilder.new.call
+  end
+end
---
+++ spec/controllers/routes_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe RoutesController do
+  before do
+    service = allow(GetCheapestEndpoint)
+      .to(receive(:new).with(origin_port: origin_port, destination_port: destination_port, sailings: anything))
+      .and_return(result)
+    # allow(service).to receive(:call).and_return(double(success?: true))
+  end
+
+  describe "GET index" do
+    context "when origin and destination ports are valid" do
+      let(:origin_port) { 'origin_port' }
+      let(:destination_port) { 'destination_port' }
+      let(:sailings) { SailingsBuilder.new.call }
+      let(:result) { double(call: double(success?: true, value!: double)) }
+
+      it "return value" do
+        get :index, params: { origin_port: origin_port, destination_port: destination_port}
+
+        expect(response.code).to eq("200")
+      end
+    end
+
+    context "when origin or destination ports are invalid" do
+      let(:origin_port) { 'origin_port' }
+      let(:destination_port) { 'destination_port' }
+      let(:sailings) { SailingsBuilder.new.call }
+      let(:result) { double(call: double(success?: false, failure: double)) }
+
+      it "return value" do
+        get :index, params: { origin_port: origin_port, destination_port: destination_port}
+
+        expect(response.code).to eq("400")
+      end
+    end
+  end
+end
